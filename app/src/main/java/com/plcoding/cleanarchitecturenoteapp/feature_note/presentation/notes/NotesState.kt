310735b70b8a020b51416ded31ee2307b4ced409
slatqh <slatqh@gmail.com>
2023-06-11 17:12:28 -0400

    update

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesState.kt
@@ -0,0 +1,11 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.utils.NoteOrder
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.utils.OrderType
+
+data class NotesState(
+    val notes: List<Note> = emptyList(),
+    val noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    val isOrderSectionVisible: Boolean = false
+)
